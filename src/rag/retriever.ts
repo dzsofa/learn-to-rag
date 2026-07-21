c4e3eec5772b03b97b83ff746277db3db22697c5
Zsofia Dedeo <Zsofia_Dedeo@epam.com>
2026-07-21 21:17:56 +0200

    feat: add retriever

---
+++ src/rag/retriever.ts
@@ -0,0 +1,39 @@
+import { Where } from 'chromadb';
+import { config } from '../config';
+import { createEmbeddings } from '../llm/ollamaEmbeddings';
+import { getCollection } from './chromaClient';
+import { Document } from '@langchain/core/documents';
+
+export async function retrieve(
+  query: string,
+  k?: number | undefined,
+  filter?: Where
+) {
+  const topK = k ?? config.retrieval.k;
+  const embeddings = createEmbeddings();
+  const queryEmb = await embeddings.embedQuery(query);
+  const collection = await getCollection();
+  const results = await collection.query({
+    queryEmbeddings: [queryEmb],
+    nResults: topK,
+    include: ['documents', 'metadatas', 'distances'],
+    where: filter
+  });
+
+  let retrieved: { document: Document; distance: number }[] = [];
+  results.documents[0].forEach((text, index) => {
+    retrieved.push({
+      document: new Document({
+        pageContent: text ?? '',
+        metadata: results.metadatas[0][index] ?? {}
+      }),
+      distance: results.distances?.[0]?.[index] ?? Infinity
+    });
+  });
+
+  return retrieved;
+}
+
+const isMain = process.argv[1]?.endsWith('retriever.ts');
+if (isMain)
+  retrieve('Why does Raskolnikov confess to the murder?').then(console.log);
