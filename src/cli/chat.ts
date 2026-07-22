77c3b90616dde91a135b20d500c152f6da71472e
Zsofia Dedeo <Zsofia_Dedeo@epam.com>
2026-07-22 14:55:32 +0200

    feat: add CLI chat

---
+++ src/cli/chat.ts
@@ -0,0 +1,26 @@
+import * as readline from 'node:readline/promises';
+import { runQuery } from '../graph/bookGraph';
+
+async function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  console.log(
+    `This is your library assistant to get information about Fyodor Dostoyevsky's Crime and Punishment. You can ask questions and I will provide answers based on the content of the book with citations,`
+  );
+  while (true) {
+    const line = await rl.question('You: ');
+    if (line.trim() === 'exit' || line.trim() === 'break') break;
+    if (line.trim() === '') continue;
+    const result = await runQuery(line);
+    console.log('\n\n');
+    console.log(result.answer);
+    console.log(result.citations.join(', '));
+  }
+
+  rl.close();
+}
+
+main().catch(console.error);
